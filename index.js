08a4958cc444ee3b9595d2a908324ed8e156fa8a
Gustavo Baeza Avello <gustavobaezaavello@gmail.com>
2024-12-06 18:32:27 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const cors = require('cors')
+const express = require('express')
+const mongoose = require('mongoose')
+
+const config = require('./utils/config')
+
+const app = express()
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+
+const mongoUrl = config.MONGODB_URI
+mongoose
+  .connect(mongoUrl)
+  .then(() => { console.log('Connected to MongoDB') })
+  .catch((error) => {
+    console.error(error)
+  })
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+app.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`)
+})
---
+++ readme.md
@@ -0,0 +1,14 @@
+# blogs-backend
+
+Blogs backend app
+
+## Iniciar
+
+```sh
+npm clone https://github.com/gus25888/
+npm install
+```
+
+Luego, crear un archivo .env en la raíz, con las siguientes variables:
+    MONGODB_URI
+    PORT
